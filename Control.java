cfa6d90584c60479239f14976b5b30eff8cf3048
CaseyOgbevoen <C16315013@mydit.ie>
2018-04-11 13:10:35 +0100

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,14 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class Control {
+    public static void main(String args[]) {
+        ArrayList List;
+        FileReader F1 = new FileReader("temp.txt");
+
+        F1.openFile();
+        List = F1.readAll();
+        System.out.println(List);
+    }
+}
---
+++ FileReader.java
@@ -0,0 +1,40 @@
+package Classes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+    public class FileReader {
+        String fileName;
+        File file;
+
+        public FileReader(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public void openFile() {
+            this.file = new File(this.fileName);
+        }
+
+        public ArrayList readAll() {
+            ArrayList totalFile = new ArrayList();
+
+            try {
+                Scanner myScanner = new Scanner(this.file);
+                myScanner.useDelimiter(" ");
+
+                while(myScanner.hasNext()) {
+                    String word = myScanner.next();
+                    totalFile.add(word);
+                }
+
+                myScanner.close();
+            } catch (FileNotFoundException var4) {
+                System.out.println(var4.getMessage());
+            }
+
+            return totalFile;
+        }
+    }
+
